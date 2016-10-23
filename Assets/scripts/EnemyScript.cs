c1c7c42d1b6bd198031fc047fce9efefa6f471d3
Yi YIN <kthtes@gmail.com>
2016-10-23 12:02:04 -0700

    Prototype 2 Added control of player Added a stupid AI for enemy Added boundary box & walls

---
+++ Assets/scripts/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour
+{  //Enemy movement
+    public float moveSpeed = 2.0f;
+    public float size = 0.3f;
+    public PlayerScript player;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = player.position();
+        Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+        float dx = pos.x - myPos.x;
+        float dy = pos.y - myPos.y;
+        Rigidbody2D rb;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(dx < 0 ? -moveSpeed : moveSpeed, dy < 0 ? -moveSpeed : moveSpeed);
+    }
+}
---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{  //Player movement
+    public float moveSpeed = 2.0f;
+    public float size = 0.1f;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    void onDirection(int d, bool move=true)
+    {
+        Rigidbody2D rb;
+        rb = GetComponent<Rigidbody2D>();
+        float[ , ] speeds = new float[4, 2] { {0, moveSpeed}, {moveSpeed,0 }, {0,-moveSpeed }, {-moveSpeed,0 } };
+        if (move)
+            rb.velocity = new Vector2(speeds[d, 0] != 0 ? speeds[d, 0] : rb.velocity.x, speeds[d, 1] != 0 ? speeds[d, 1] : rb.velocity.y);
+        else
+            rb.velocity = new Vector2(speeds[d, 0] != 0 ? 0 : rb.velocity.x, speeds[d, 1] != 0 ? 0 : rb.velocity.y);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            onDirection(0);
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow))
+            onDirection(0, false);
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            onDirection(1);
+        else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+            onDirection(1, false);
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            onDirection(2);
+        else if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+            onDirection(2, false);
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            onDirection(3);
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+            onDirection(3, false);
+
+    }
+    public Vector2 position()
+    {
+        return new Vector2(transform.position.x, transform.position.y);
+    }
+}
