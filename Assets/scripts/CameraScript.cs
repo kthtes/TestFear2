096cce17fee253b8aed07c0ea23388b8a2397c3d
Yi YIN <kthtes@gmail.com>
2016-10-24 16:22:14 -0700

    Added camera following in stageTest

---
+++ Assets/scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+	// "player" to follow
+	public PlayerScript player;
+
+	// Use this for initialization
+	void Start() {
+	}
+
+	void follow(Vector2 pos)
+	{
+		Camera camera = GetComponent<Camera>();
+		Vector3 viewPoint = camera.WorldToViewportPoint(new Vector3(pos.x, pos.y, 0));
+		Vector3 worldPoint = camera.ViewportToWorldPoint(viewPoint);
+		transform.position = new Vector3(worldPoint.x, worldPoint.y, transform.position.z);
+	}
+	void zoom(int level, float time)
+	{
+
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		follow(player.position());
+	}
+
+}
