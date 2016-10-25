9da266a14315f63a8193c4c77565d95cb1060318
Yi YIN <kthtes@gmail.com>
2016-10-25 12:51:28 -0700

    Added "Eat" function. Basic functions all done.

--- Assets/scripts/EnemyScript.cs
+++ Assets/scripts/EnemyScript.cs
@@ -7,20 +7,19 @@ public class EnemyScript :  PlayerScript
     public YouScript player;
 	NavMeshAgent2D nav;
 
-    // Use this for initialization
-    void Start()
-    {
-		nav = GetComponent<NavMeshAgent2D>();
-		nav.destination = player.transform.position;
+	void Start()
+	{
+		setSizeLevel(sizeLevel);
+		nav= GetComponent<NavMeshAgent2D>();
+		nav.destination = player.pos2();
 	}
-
 	// Update is called once per frame
 	void Update()
     {
 		adjustFace();
     }
 	void adjustFace()
-	{
+	{		
 		float theta = Mathf.Atan2(nav.velocity.y, nav.velocity.x);
 		transform.rotation = Quaternion.Euler(0, 0, theta * 180.0f / Mathf.PI);
 	}
@@ -37,9 +36,22 @@ public class EnemyScript :  PlayerScript
 	// we do collision detecting only in enemy, since enemy eats enemy and You
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		
+		PlayerScript other = coll.gameObject.GetComponent<PlayerScript>();
+		if (other == null)
+			return;
+		Debug.Log("Eat! size:[" + sizeLevel+"] VS size:["+other.sizeLevel);
 		// 1. the larger one: grow
-		
+		if (other.sizeLevel > sizeLevel)
+		{
+			Debug.Log("Eat! will destroy:"+sizeLevel);
+			other.grow();
+			Destroy(gameObject);
+		}else
+		{
+			Debug.Log("Eat! will destroy:" + other.sizeLevel);
+			Destroy(other.gameObject);
+			grow();
+		}
 		// 2. the smaller one: destroy
 	}
 }
